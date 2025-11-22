4478bc80029e6d103564871c83a430cffd89ec9e
Abinas <rak51259@gmail.com>
2025-11-22 17:55:16 +0600

    api called added on server

--- js/script.js
+++ js/script.js
@@ -1,28 +1,104 @@
 $(function () {
+    $('#main').load('html/home.html');
 
     $('#logo').click(function () {
         location.reload();
+
+
+
     });
 
-    $('#toploginbtn, #downloginbtn').click(function () {
-        $('#loginform').removeClass('d-none');
-        $('#products').addClass('d-none');
-    });
 
-    $('#btngroup .brand-btn').click(function(){
+    $('#btngroup .brand-btn').click(function () {
         $('#btngroup .brand-btn').removeClass('active-btn');
         $(this).addClass('active-btn');
 
     });
 
-    $('#samsung, #Samsung').click(function(){
-        $('#products, #loginform').addClass('d-none');
-        $('#sampro').removeClass('d-none');
+function loadpage(page) {
+
+    const parts = page.split(" ");
+    const file = parts[0];         
+    const selector = parts[1];      
+
+    $.ajax({
+        url: file,
+        method: 'GET',
+        dataType: 'html',
+        success: function (response) {
+            if (selector) {
+                let selectedPart = $(response).find(selector);
+                $("#main").html(selectedPart);
+            } else {
+                $("#main").html(response);
+            }
+        },
+        error: function (xhr, status, error) {
+            console.error('API call failed:', status, error);
+        },
+        complete: function () {
+            console.log('API call completed.');
+        }
+    });
+}
+
+
+
+    $('#downloginbtn, #toploginbtn').click(function () {
+
+        loadpage('html/login.html');
     });
 
 
+    $('#samsung, #Samsung').click(function () {
+
+        loadpage('html/samsung.html');
+
+    });
+
+    $('#xiaomi, #Xiaomi').click(function () {
+
+        loadpage('html/home.html #xiaomi');
+
+    });
+
+    $('#oppo, #Oppo').click(function () {
+
+        loadpage('html/home.html #oppo');
+
+    });
+
+    $('#vivo, #Vivo').click(function () {
+
+        loadpage('html/home.html #vivo');
+
+    });
+
+    $('#realme, #Realme').click(function () {
+
+        loadpage('html/home.html #realme');
+
+    });
+
+    $('#huawei, #Huawei').click(function () {
+
+        loadpage('html/home.html #huawei');
+
+    });
+
+    $('#infinix, #Infinix').click(function () {
+
+        loadpage('html/home.html #infinix');
+
+    });
+
+    $('#tecno, #Tecno').click(function () {
+
+        loadpage('html/home.html #tecno');
+
+    });
 
 })
 
-// set current year in footer
+// year in footer
 document.getElementById('year').textContent = new Date().getFullYear();
